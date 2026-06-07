471271ff8ed2f77edb8ac331d71ff68e67a6d3cc
gosth <augustusdm8@gmail.com>
2026-06-07 18:39:28 -0500

    feat: create ecs execution role with ecr and cloudwatch permissions

--- iac/modules/security/iam/main.tf
+++ iac/modules/security/iam/main.tf
@@ -8,3 +8,15 @@ data "aws_iam_policy_document" "ecs_tasks_trust_policy" {
     }
   }
 }
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "${var.project_name}-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_trust_policy.json
+
+}
+
+# Política para permitir a hacer pull de ECR y escribir logs 
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
