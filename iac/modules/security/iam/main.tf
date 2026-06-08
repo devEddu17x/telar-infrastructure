cc50b824f0fad7453219023233f46f2721606a65
gosth <augustusdm8@gmail.com>
2026-06-07 20:07:07 -0500

    feat(security): implement IAM roles for ECS and Lambda

--- iac/modules/security/iam/main.tf
+++ iac/modules/security/iam/main.tf
@@ -9,33 +9,49 @@ data "aws_iam_policy_document" "ecs_tasks_trust_policy" {
   }
 }
 
+# Trust Policy para Lambda
+data "aws_iam_policy_document" "lambda_trust_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "${var.project_name}-ecs-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_trust_policy.json
-
 }
 
-# Política para permitir a hacer pull de ECR y escribir logs 
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+# Política para permitir hacer pulls a ECR y CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_managed_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 data "aws_iam_policy_document" "ecs_execution_secrets_policy" {
   statement {
+    sid    = "AllowReadSecretsManager"
     effect = "Allow"
     actions = [
       "secretsmanager:GetSecretValue",
-      "ssm:GetParameters",
-      "ssm:GetParameter"
     ]
-    resources = [
-      var.secrets_manager_arn,
-      var.ssm_parameter_arn
-    ]
-
+    resources = var.secrets_manager_arns
   }
 
+  statement {
+    sid    = "AllowReadSSMParameters"
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+    ]
+    resources = var.ssm_parameter_arns
+  }
 }
 
 resource "aws_iam_role_policy" "ecs_execution_secrets" {
@@ -47,37 +63,61 @@ resource "aws_iam_role_policy" "ecs_execution_secrets" {
 resource "aws_iam_role" "ecs_task_role" {
   name               = "${var.project_name}-ecs-task-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_trust_policy.json
-
 }
 
 data "aws_iam_policy_document" "ecs_task_policy" {
-  # Acceso a S3 
   statement {
+    sid    = "AllowS3ObjectOperations"
     effect = "Allow"
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:ListBucket",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
     ]
-    resources = [
-      var.s3_bucket_arn,
-      "${var.s3_bucket_arn}/*"
-    ]
+    resources = [for arn in var.s3_bucket_arns : "${arn}/*"]
   }
 
-
-  # Acceso a AWS X-Ray
   statement {
+    sid    = "AllowS3BucketOperations"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+    resources = var.s3_bucket_arns
+  }
+
+  statement {
+    sid    = "AllowCognitoAdminOperations"
+    effect = "Allow"
+    actions = [
+      "cognito-idp:AdminCreateUser",
+      "cognito-idp:AdminDeleteUser",
+      "cognito-idp:AdminDisableUser",
+      "cognito-idp:AdminEnableUser",
+      "cognito-idp:AdminGetUser",
+      "cognito-idp:AdminListGroupsForUser",
+      "cognito-idp:AdminAddUserToGroup",
+      "cognito-idp:AdminRemoveUserFromGroup",
+      "cognito-idp:AdminUpdateUserAttributes",
+      "cognito-idp:AdminResetUserPassword",
+      "cognito-idp:ListUsers",
+      "cognito-idp:ListGroups",
+    ]
+    resources = [var.cognito_user_pool_arn]
+  }
+
+  statement {
+    sid    = "AllowXRayTracing"
     effect = "Allow"
     actions = [
       "xray:PutTraceSegments",
       "xray:PutTelemetryRecords",
       "xray:GetSamplingRules",
       "xray:GetSamplingTargets",
-      "xray:GetSamplingStatisticSummaries"
+      "xray:GetSamplingStatisticSummaries",
     ]
-    resources = ["*"]
+    resources = ["*"] 
   }
 }
 
@@ -87,5 +127,42 @@ resource "aws_iam_role_policy" "ecs_task" {
   policy = data.aws_iam_policy_document.ecs_task_policy.json
 }
 
+resource "aws_iam_role" "lambda_pre_signup_role" {
+  name               = "${var.project_name}-lambda-pre-signup-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy.json
+}
 
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_pre_signup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
+data "aws_iam_policy_document" "lambda_pre_signup_policy" {
+  # Leer el secreto compartido que el backend generó
+  statement {
+    sid    = "AllowReadSharedSecret"
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+    resources = var.secrets_manager_arns
+  }
+
+  # Consultar el User Pool para verificar si el usuario existe / comparar código
+  statement {
+    sid    = "AllowCognitoLookup"
+    effect = "Allow"
+    actions = [
+      "cognito-idp:AdminGetUser",
+      "cognito-idp:ListUsers",
+      "cognito-idp:DescribeUserPool",
+    ]
+    resources = [var.cognito_user_pool_arn]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_pre_signup" {
+  name   = "${var.project_name}-lambda-pre-signup-policy"
+  role   = aws_iam_role.lambda_pre_signup_role.id
+  policy = data.aws_iam_policy_document.lambda_pre_signup_policy.json
+}
