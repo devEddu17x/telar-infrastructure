13fc6337f1cd83f98caa0c8f2cd4ede233d571df
gosth <augustusdm8@gmail.com>
2026-06-08 21:16:18 -0500

    feat: add internet gateway and NAT gateways, route tables for public

---
+++ iac/modules/networking/route_tables.tf
@@ -0,0 +1,56 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-public-rt"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_compute" {
+  for_each = { for az in var.availability_zones : az => az }
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[each.key].id
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-private-compute-rt-${each.key}"
+  })
+}
+
+resource "aws_route_table_association" "private_compute" {
+  for_each = aws_subnet.private_compute
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_compute[each.key].id
+}
+
+resource "aws_route_table" "private_persistence" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-private-persistence-rt"
+  })
+}
+
+resource "aws_route_table_association" "private_persistence" {
+  for_each = aws_subnet.private_persistence
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_persistence.id
+}
