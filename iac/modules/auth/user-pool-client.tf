7fbce5d859e8b662328b154084c1fbe3166d4b91
Carlos Alonso Díaz Uceda <cdiazu20@hotmail.com>
2026-06-10 17:18:15 -0500

    feat: add Cognito user pool client resource configuration

---
+++ iac/modules/auth/user-pool-client.tf
@@ -0,0 +1,21 @@
+resource "aws_cognito_user_pool_client" "frontend_client" {
+  name         = "${var.name_prefix}-frontend-client"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+  ]
+
+  access_token_validity  = 1
+  id_token_validity      = 1
+  refresh_token_validity = 30
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+
+  prevent_user_existence_errors = "ENABLED"
+}
