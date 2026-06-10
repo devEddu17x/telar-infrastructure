b01418a460bdd3ddddb1406250b1294620ffd091
alba <albamalca15@gmail.com>
2026-06-10 16:24:55 -0500

    feat: remove S3 bucket logging resource in bucket.tf and related comments

--- iac/modules/storage/bucket.tf
+++ iac/modules/storage/bucket.tf
@@ -4,13 +4,3 @@ resource "aws_s3_bucket" "this" {
 
   tags = var.tags
 }
-
-#logging
-
-resource "aws_s3_bucket_logging" "this" {
-  count = var.logging_target_bucket != null ? 1 : 0
-
-  bucket        = aws_s3_bucket.this.id
-  target_bucket = var.logging_target_bucket
-  target_prefix = var.logging_target_prefix
-}
