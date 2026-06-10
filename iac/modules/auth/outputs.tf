7f7f7828fb648268f70246eaebd0364723fa4f37
Carlos Alonso Díaz Uceda <cdiazu20@hotmail.com>
2026-06-10 17:18:26 -0500

    feat: add outputs for Cognito User Pool and frontend client ID

---
+++ iac/modules/auth/outputs.tf
@@ -0,0 +1,19 @@
+output "user_pool_id" {
+  description = "ID of the Cognito User Pool"
+  value       = aws_cognito_user_pool.pool.id
+}
+
+output "user_pool_arn" {
+  description = "ARN of the Cognito User Pool"
+  value       = aws_cognito_user_pool.pool.arn
+}
+
+output "user_pool_endpoint" {
+  description = "Endpoint of the Cognito User Pool"
+  value       = aws_cognito_user_pool.pool.endpoint
+}
+
+output "frontend_client_id" {
+  description = "Client ID of the User Pool Client for the frontend"
+  value       = aws_cognito_user_pool_client.frontend_client.id
+}
