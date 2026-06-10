dc0a3db1c10ccfa3162cacb5546787c219f30288
gosth <augustusdm8@gmail.com>
2026-06-10 12:19:11 -0500

    refactor: remove NAT gateway to isolate ECS

--- iac/modules/networking/gateways.tf
+++ iac/modules/networking/gateways.tf
@@ -5,28 +5,3 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.name_prefix}-igw"
   })
 }
-
-resource "aws_eip" "nat" {
-  for_each = { for az in var.availability_zones : az => az }
-
-  domain = "vpc"
-
-  tags = merge(var.tags, {
-    Name = "${var.name_prefix}-nat-eip-${each.key}"
-  })
-
-  depends_on = [aws_internet_gateway.main]
-}
-
-resource "aws_nat_gateway" "main" {
-  for_each = { for az in var.availability_zones : az => az }
-
-  allocation_id = aws_eip.nat[each.key].id
-  subnet_id     = aws_subnet.public[each.key].id
-
-  tags = merge(var.tags, {
-    Name = "${var.name_prefix}-nat-gw-${each.key}"
-  })
-
-  depends_on = [aws_internet_gateway.main]
-}
--- iac/modules/networking/route_tables.tf
+++ iac/modules/networking/route_tables.tf
@@ -23,11 +23,6 @@ resource "aws_route_table" "private_compute" {
 
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[each.key].id
-  }
-
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-private-compute-rt-${each.key}"
   })
