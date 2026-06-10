091ab254a048062f6547500fffb5ac7608c97000
alba <albamalca15@gmail.com>
2026-06-10 17:28:56 -0500

    feat: add ECR repository and associated variables for configuration

---
+++ iac/modules/repository/repository.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+
+  encryption_configuration {
+    encryption_type = var.encryption_type
+    kms_key         = var.encryption_type == "KMS" ? var.kms_key_id : null
+  }
+
+  tags = var.tags
+}
