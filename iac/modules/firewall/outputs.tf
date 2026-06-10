43d271505855fd3390aabc4a038562d3a8bd71d6
yzrlvu <cindymontoyagranda123@gmail.com>
2026-06-10 15:07:57 -0500

    feat: add outputs for WAFv2 Web ACL in firewall module

---
+++ iac/modules/firewall/outputs.tf
@@ -0,0 +1,19 @@
+output "web_acl_arn" {
+  description = "ARN of the WAFv2 Web ACL. Pass this to the api_gateway module (waf_web_acl_arn) to associate it with the stage"
+  value       = aws_wafv2_web_acl.main.arn
+}
+
+output "web_acl_id" {
+  description = "ID of the WAFv2 Web ACL"
+  value       = aws_wafv2_web_acl.main.id
+}
+
+output "web_acl_name" {
+  description = "Name of the WAFv2 Web ACL"
+  value       = aws_wafv2_web_acl.main.name
+}
+
+output "web_acl_capacity" {
+  description = "Web ACL Capacity Units (WCU) currently consumed by the configured rules"
+  value       = aws_wafv2_web_acl.main.capacity
+}
