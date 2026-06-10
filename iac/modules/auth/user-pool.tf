1cf9a2e39e106b9e1f3c2d09530b384d97081b5f
Carlos Alonso Díaz Uceda <cdiazu20@hotmail.com>
2026-06-10 17:18:09 -0500

    feat: add Cognito user pool resource configuration

---
+++ iac/modules/auth/user-pool.tf
@@ -0,0 +1,39 @@
+resource "aws_cognito_user_pool" "pool" {
+
+  name = "${var.name_prefix}-user-pool"
+
+  auto_verified_attributes = ["email"]
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject        = "${var.app_email_subject} - Verify your email"
+    email_message        = "Your verification code is {####}"
+  }
+
+  password_policy {
+    minimum_length                   = 12
+    require_uppercase                = true
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    temporary_password_validity_days = 7
+  }
+
+  schema {
+    name                = "email"
+    required            = true
+    mutable             = false
+    attribute_data_type = "String"
+  }
+
+  schema {
+    name                = "tenant_id"
+    mutable             = false
+    required            = false
+    attribute_data_type = "String"
+  }
+
+  lambda_config {
+    pre_sign_up = var.pre_signup_lambda_arn
+  }
+}
