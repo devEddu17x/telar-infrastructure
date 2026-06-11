30c24b9f0a431acec9a1f5168ca9b033ff969f58
Carlos Alonso Díaz Uceda <cdiazu20@hotmail.com>
2026-06-10 23:56:48 -0500

    feat: add AWS Lambda permission for Cognito invocation

---
+++ iac/modules/compute/lambda/permission.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_permission" "allow_cognito" {
+  statement_id  = "AllowCognitoInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = var.cognito_user_pool_arn
+}
