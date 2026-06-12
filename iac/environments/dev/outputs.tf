5719d02636a6793665c5d43f8c48dfd617ee3e87
eddu <azabacheeduardo85@gmail.com>
2026-06-12 17:00:55 -0500

    feat: add outputs for WAFv2 Web ACL in dev environment
    Co-authored-by: yzrlvu <yzrlvu@users.noreply.github.com>
    
    Co-authored-by: yzrlvu <yzrlvu@users.noreply.github.com>

--- iac/environments/dev/outputs.tf
+++ iac/environments/dev/outputs.tf
@@ -17,3 +17,23 @@ output "cognito_frontend_client_id" {
   description = "Client ID of the User Pool Client for the frontend"
   value       = module.auth.frontend_client_id
 }
+
+output "firewall_api_web_acl_arn" {
+  description = "ARN of the WAFv2 Web ACL."
+  value       = module.firewall_api.web_acl_arn
+}
+
+output "firewall_api_web_acl_id" {
+  description = "ID of the WAFv2 Web ACL"
+  value       = module.firewall_api.web_acl_id
+}
+
+output "firewall_api_web_acl_name" {
+  description = "Name of the WAFv2 Web ACL"
+  value       = module.firewall_api.web_acl_name
+}
+
+output "firewall_api_web_acl_capacity" {
+  description = "Web ACL Capacity Units (WCU) consumed by the configured rules"
+  value       = module.firewall_api.web_acl_capacity
+}
