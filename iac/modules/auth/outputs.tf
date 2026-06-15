5d3e4a72d7b45e6efb6e6e77acb0f188b91a1645
eddu <azabacheeduardo85@gmail.com>
2026-06-15 09:31:12 -0500

    feat: add user pool issuer URL output to Cognito module

--- iac/modules/auth/outputs.tf
+++ iac/modules/auth/outputs.tf
@@ -17,3 +17,8 @@ output "frontend_client_id" {
   description = "Client ID of the User Pool Client for the frontend"
   value       = aws_cognito_user_pool_client.frontend_client.id
 }
+
+output "user_pool_issuer_url" {
+  description = "Issuer URL of the Cognito User Pool"
+  value       = "https://cognito-idp.${data.aws_region.current.region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
+}
