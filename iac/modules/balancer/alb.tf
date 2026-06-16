237dbe6e1edd1d70f5b947d956cfc8c3fdf02a0b
alba <albamalca15@gmail.com>
2026-06-15 21:34:11 -0500

    fix: enable deletion protection for ALB

--- iac/modules/balancer/alb.tf
+++ iac/modules/balancer/alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "alb" {
   load_balancer_type         = "application"
   security_groups            = var.security_group_ids
   subnets                    = var.subnet_ids
-  enable_deletion_protection = var.enable_deletion_protection
+  enable_deletion_protection = true
 
   access_logs {
     enabled = true
