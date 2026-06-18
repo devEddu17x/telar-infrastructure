1b8c079d973aee5c14b004bf483c633ebf691ebd
eddu <azabacheeduardo85@gmail.com>
2026-06-18 04:12:17 -0500

    style: format alignment of resource attributes in alb.tf

--- iac/modules/balancer/alb.tf
+++ iac/modules/balancer/alb.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "alb" {
-  name                       = "${var.name_prefix}-alb"
-  internal                   = true
-  load_balancer_type         = "application"
-  security_groups            = var.security_group_ids
-  subnets                    = var.subnet_ids
+  name               = "${var.name_prefix}-alb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.subnet_ids
 
   enable_deletion_protection = true
   drop_invalid_header_fields = true
