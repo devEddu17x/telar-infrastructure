1298c614f64c66e5b49f5d7798abe4151cd1d2e1
eddu <azabacheeduardo85@gmail.com>
2026-06-18 04:10:50 -0500

    feat: add egress rule to allow HTTPS traffic to S3 via gateway endpoint

--- iac/modules/networking/security_groups.tf
+++ iac/modules/networking/security_groups.tf
@@ -74,6 +74,16 @@ resource "aws_vpc_security_group_egress_rule" "ecs_to_aurora" {
   referenced_security_group_id = aws_security_group.aurora.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "ecs_to_s3" {
+  security_group_id = aws_security_group.ecs_tasks.id
+
+  description    = "Allow HTTPS to S3 via gateway endpoint for ECR image layers"
+  from_port      = 443
+  to_port        = 443
+  ip_protocol    = "tcp"
+  prefix_list_id = aws_vpc_endpoint.s3.prefix_list_id
+}
+
 resource "aws_security_group" "apigw_vpc_link" {
   name        = "${var.name_prefix}-apigw-vpc-link-sg"
   description = "Security group for API Gateway VPC Link to ALB"
