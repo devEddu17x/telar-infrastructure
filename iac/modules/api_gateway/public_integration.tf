dfd2390c0f8f978e6a203b7d6468185a28bc436f
eddu <azabacheeduardo85@gmail.com>
2026-06-18 22:02:36 -0500

    fix: update parent_id for auth resource and enhance URI for auth proxy integration

--- iac/modules/api_gateway/public_integration.tf
+++ iac/modules/api_gateway/public_integration.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_resource" "auth" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  parent_id   = aws_api_gateway_resource.v1.id
   path_part   = "auth"
 }
 
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "auth_proxy" {
   http_method             = aws_api_gateway_method.auth_proxy.http_method
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
-  uri                     = "http://${var.alb_dns_name}/auth/{proxy}"
+  uri                     = "http://${var.alb_dns_name}/${var.api_prefix}/${var.api_version}/auth/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_apigatewayv2_vpc_link.link_to_alb.id
   integration_target      = var.alb_arn
