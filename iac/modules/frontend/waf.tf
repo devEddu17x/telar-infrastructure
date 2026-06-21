9d42b670eeae4449e797dc96d77c1df9b6f276a3
eddu <azabacheeduardo85@gmail.com>
2026-06-21 12:25:54 -0500

    chore: remove deprecated frontend module

--- iac/modules/frontend/waf.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_wafv2_web_acl_association" "amplify_frontend" {
-  count = var.web_acl_arn != "" ? 1 : 0
-
-  resource_arn = aws_amplify_app.frontend.arn
-  web_acl_arn  = var.web_acl_arn
-
-  depends_on = [aws_amplify_app.frontend]
-}
