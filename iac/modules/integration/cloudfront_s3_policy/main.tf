d8973f33df1fa5c4541ca26af1285253363d1b94
eddu <azabacheeduardo85@gmail.com>
2026-06-21 12:20:40 -0500

    feat(cdn): add S3 bucket policy for CloudFront integration with HTTPS enforcement

---
+++ iac/modules/integration/cloudfront_s3_policy/main.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket_policy" "this" {
+  bucket = var.bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnforceHTTPSOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          var.bucket_arn,
+          "${var.bucket_arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+      {
+        Sid    = "AllowCloudFrontOAC"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${var.bucket_arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = var.distribution_arn
+          }
+        }
+      },
+    ]
+  })
+}
