edfb3e6c4b13df88a10feda0757862de883b0c2c
eddu <azabacheeduardo85@gmail.com>
2026-06-21 19:56:40 -0500

    feat(oidc): add outputs for GitHub OIDC provider and deployment roles

---
+++ iac/shared/outputs.tf
@@ -0,0 +1,24 @@
+output "github_oidc_provider_arn" {
+  description = "ARN of the GitHub OIDC provider"
+  value       = aws_iam_openid_connect_provider.github.arn
+}
+
+output "frontend_github_oidc_role_arn" {
+  description = "ARN of the frontend deployment role"
+  value       = module.frontend_github_oidc_role.role_arn
+}
+
+output "frontend_github_oidc_role_name" {
+  description = "Name of the frontend deployment role"
+  value       = module.frontend_github_oidc_role.role_name
+}
+
+output "backend_github_oidc_role_arn" {
+  description = "ARN of the backend deployment role"
+  value       = module.backend_github_oidc_role.role_arn
+}
+
+output "backend_github_oidc_role_name" {
+  description = "Name of the backend deployment role"
+  value       = module.backend_github_oidc_role.role_name
+}
