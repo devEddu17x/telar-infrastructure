cacbc96ba0151e3af974a75ea06f7475af25cdc9
yzrlvu <cmontoyag1@upao.edu.pe>
2026-06-25 23:25:42 -0500

    fix: [CKV_AWS_2] Ensure ALB protocol is HTTPS

--- iac/modules/balancer/listener.tf
+++ iac/modules/balancer/listener.tf
@@ -1,4 +1,5 @@
 resource "aws_lb_listener" "alb_listener" {
+  #checkov:skip=CKV_AWS_2:The ALB uses HTTP because it is in a VPC and connects to the API through a vpclink
   load_balancer_arn = aws_lb.alb.arn
   port              = var.alb.port
   protocol          = var.alb.protocol
