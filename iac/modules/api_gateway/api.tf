2172996404aaa0d085629f3a4fbd77676a65f253
eddu <azabacheeduardo85@gmail.com>
2026-06-27 13:29:50 -0500

    fix: [CKV_AWS_59] Ensure there is no open access to back-end resources through API

--- iac/modules/api_gateway/api.tf
+++ iac/modules/api_gateway/api.tf
@@ -7,7 +7,7 @@ resource "aws_api_gateway_rest_api" "main" {
 
   tags = var.tags
 
-   lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
--- iac/modules/api_gateway/public_integration.tf
+++ iac/modules/api_gateway/public_integration.tf
@@ -11,6 +11,8 @@ resource "aws_api_gateway_resource" "auth_proxy" {
 }
 
 resource "aws_api_gateway_method" "auth_proxy" {
+  #checkov:skip=CKV_AWS_59:Auth routes must stay public for signup and login flows.
+
   rest_api_id   = aws_api_gateway_rest_api.main.id
   resource_id   = aws_api_gateway_resource.auth_proxy.id
   http_method   = "ANY"
