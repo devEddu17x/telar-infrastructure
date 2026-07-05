9d42d7a4cfc22dac5f632ee64a62230256790ea8
eddu <azabacheeduardo85@gmail.com>
2026-07-05 12:41:28 -0500

    feat(oidc): add IAM policy documents for role assumption and SES email sending

---
+++ iac/modules/oidc/iac_role/policies.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [var.provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${var.provider_url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "${var.provider_url}:sub"
+      values   = concat([for branch in var.branches : "repo:${var.repository}:ref:refs/heads/${branch}"], [for environment in var.environments : "repo:${var.repository}:environment:${environment}"])
+    }
+  }
+}
+
+data "aws_iam_policy_document" "deploy" {
+  statement {
+    sid = "SendCheckovReports"
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail",
+    ]
+    resources = var.ses_identity_arns
+  }
+}
