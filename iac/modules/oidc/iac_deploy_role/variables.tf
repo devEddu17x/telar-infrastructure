a870f1482f611cc16ba52d1aa480dd25c884c797
eddu <azabacheeduardo85@gmail.com>
2026-07-09 02:41:44 -0500

    feat: add variables for OIDC deploy role configuration

---
+++ iac/modules/oidc/iac_deploy_role/variables.tf
@@ -0,0 +1,53 @@
+variable "name_prefix" {
+  description = "Project-environment prefix for resources"
+  type        = string
+}
+
+variable "role_suffix" {
+  description = "Suffix for the IAM role name"
+  type        = string
+}
+
+variable "provider_arn" {
+  description = "ARN of the GitHub OIDC provider"
+  type        = string
+}
+
+variable "provider_url" {
+  description = "Host name of the GitHub OIDC provider"
+  type        = string
+}
+
+variable "repository" {
+  description = "GitHub repository allowed to assume the role"
+  type        = string
+}
+
+variable "branches" {
+  description = "Git branches allowed to assume the role"
+  type        = list(string)
+  default     = []
+}
+
+variable "environments" {
+  description = "GitHub environments allowed to assume the role"
+  type        = list(string)
+  default     = []
+}
+
+variable "managed_policy_arns" {
+  description = "Managed policies attached to the deploy role"
+  type        = list(string)
+  default     = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+variable "from_addresses" {
+  description = "Email addresses allowed as SES senders"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Common tags applied to IAM resources"
+  type        = map(string)
+  default     = {}
+}
