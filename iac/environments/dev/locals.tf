cbf2e2929008824cc16c73151402435c29c9a67a
eddu <azabacheeduardo85@gmail.com>
2026-07-18 17:41:57 -0500

    feat: add upper_project_name local variable for formatted project name

--- iac/environments/dev/locals.tf
+++ iac/environments/dev/locals.tf
@@ -6,4 +6,6 @@ locals {
   }
 
   name_prefix = "${var.project_name}-${var.environment}"
+
+  upper_project_name = "${upper(substr(var.project_name, 0, 1))}${substr(var.project_name, 1, length(var.project_name) - 1)}"
 }
